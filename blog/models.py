8b338cedc97025c41ed91113cff36e0f8777ee35
Ilya <ilyaprikaz@gmail.com>
2021-03-31 11:56:36 +0400

    Add blog page and blog functionality

--- blog/models.py
+++ blog/models.py
@@ -5,4 +5,4 @@ class Post(models.Model):
     title = models.CharField(max_length=300)
     date = models.DateTimeField()
     text = models.TextField()
-    image =models.ImageField(upload_to='event_images/')
+    image = models.ImageField(upload_to='event_images/')
